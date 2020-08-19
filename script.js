8cfad22d02e2315707d8a359ba363e6f8b3483a1
Sandesh Nangare <sandesh682@gmail.com>
2020-08-19 11:49:59 +0530

    Todo list Complete

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ let form = document.querySelector('body > header > div > div > div > div > form.
 let ul = document.querySelector('body > header > div > div > div > div > ul');
 let search = document.querySelector('body > header > div > div > div > div > form.form-group.mx-auto.my-3 > input');
 
-
 let generateTemplate = (val) => {
 
     ul.innerHTML += `<li class="list-group-item bg-dark">${val} <i class="fas fa-trash pointer"></i></li>`;
@@ -30,28 +29,13 @@ ul.addEventListener('click', e => {
 search.addEventListener('keyup', e =>{
     
     val = e.target.value;
-    let li = document.querySelectorAll('li');
-    li = Array.from(li);
+    
+    Array.from(ul.children)
+      .filter(li => !li.innerText.includes(val))
+      .forEach( item => item.classList.add('d-none'));
 
-    let new_li = li.filter(tag => {
-        if(tag.innerText.includes(val))
-          {
-            return true;   
-          }
-          else{
-              return false;
-          }
-    });
-    new_li = Array.from(new_li);
-   
-    li.forEach( event => {
-
-        new_li.forEach(event1 => {
-            if(event != event1){
-                console.log(event);
-                console.log(event1);
-                event.classList.add('d-none');
-            }
-        });
-    });
+    Array.from(ul.children)
+      .filter(li => li.innerText.includes(val))
+      .forEach( item => item.classList.remove('d-none'));
+    
 } );
